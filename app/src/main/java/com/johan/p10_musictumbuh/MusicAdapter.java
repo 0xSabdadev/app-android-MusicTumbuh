94af5bf17dbfcfc0b711a0b2486481340bfa0c37
0xSabdadev <jasonalhilal@gmail.com>
2022-11-03 16:09:16 +0700

    start play activity

--- app/src/main/java/com/johan/p10_musictumbuh/MusicAdapter.java
+++ app/src/main/java/com/johan/p10_musictumbuh/MusicAdapter.java
@@ -1,6 +1,7 @@
 package com.johan.p10_musictumbuh;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.view.LayoutInflater;
@@ -46,6 +47,13 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MyVieHolder>
                     .load(R.drawable.app_logo)
                     .into(holder.album_art);
         }
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext,PlayerActivity.class);
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
